023872ccc35540e2881aeb4509ec6fc704f82cf8
michelle.taal@shell.com <michelle.taal@shell.com>
2023-05-11 18:34:31 +1000

    update test output

--- tests/package_test.py
+++ tests/package_test.py
@@ -10,10 +10,12 @@ project_root = os.path.join(current_dir, os.pardir)
 output_dir = os.path.join(current_dir, 'output')
 example_files_dir = os.path.join(current_dir, 'mend_example_results')
 failed_mend = os.path.join(example_files_dir, 'failed_mend.json')
-failed_mend_with_invalid_lines = os.path.join(example_files_dir, 'failed_mend_with_invalid_lines.json')
+failed_mend_with_invalid_lines = os.path.join(
+    example_files_dir, 'failed_mend_with_invalid_lines.json')
 valid_mend = os.path.join(example_files_dir, 'valid_mend.json')
 
-junit_conversor_cli = os.path.join(current_dir, os.pardir, 'bin', 'junit_conversor')
+junit_conversor_cli = os.path.join(
+    current_dir, os.pardir, 'bin', 'junit_conversor')
 
 
 class TestCase(unittest.TestCase):
@@ -28,10 +30,12 @@ class TestCase(unittest.TestCase):
                             % content[0:30])
 
     def assertFileExist(self, file_name):
-        self.assertTrue(os.path.exists(file_name), 'File %s does not exist' % file_name)
+        self.assertTrue(os.path.exists(file_name),
+                        'File %s does not exist' % file_name)
 
     def assertFileDoesNotExist(self, file_name):
-        self.assertFalse(os.path.exists(file_name), 'File %s exist' % file_name)
+        self.assertFalse(os.path.exists(file_name),
+                         'File %s exist' % file_name)
 
 
 class ParseTest(TestCase):
@@ -39,9 +43,9 @@ class ParseTest(TestCase):
         parsed = _parse(failed_mend)
 
         self.assertEqual(parsed, {
-            'CVE-2022-42969:py-1.11.0-py2.py3-none-any.whl': [
-                {'name': 'CVE-2022-42969:py-1.11.0-py2.py3-none-any.whl', 'file': 'py-1.11.0-py2.py3-none-any.whl', 'code': 'CVE', 'severity': 'HIGH', 'score': 7.5, 'detail': 'The py library through 1.11.0 for Python allows remote attackers to conduct a ReDoS (Regular expression Denial of Service) attack via a Subversion repository with crafted info data, because the InfoSvnCommand argument is mishandled.'}]
-                })
+            'py-1.11.0-py2.py3-none-any.whl:CVE-2022-42969': [
+                {'name': 'py-1.11.0-py2.py3-none-any.whl:CVE-2022-42969', 'file': 'py-1.11.0-py2.py3-none-any.whl', 'code': 'CVE', 'severity': 'HIGH', 'score': 7.5, 'detail': 'The py library through 1.11.0 for Python allows remote attackers to conduct a ReDoS (Regular expression Denial of Service) attack via a Subversion repository with crafted info data, because the InfoSvnCommand argument is mishandled.'}]
+        })
 
     def test_should_return_an_empty_dict_when_parsing_a_mend_success_file(self):
         self.assertEqual({}, _parse(valid_mend))
@@ -50,9 +54,9 @@ class ParseTest(TestCase):
         parsed = _parse(failed_mend_with_invalid_lines)
 
         self.assertEqual(parsed, {
-            'CVE-2022-42969:py-1.11.0-py2.py3-none-any.whl': [
-                {'name': 'CVE-2022-42969:py-1.11.0-py2.py3-none-any.whl', 'file': 'py-1.11.0-py2.py3-none-any.whl', 'code': 'CVE', 'severity': 'HIGH', 'score': 7.5, 'detail': 'The py library through 1.11.0 for Python allows remote attackers to conduct a ReDoS (Regular expression Denial of Service) attack via a Subversion repository with crafted info data, because the InfoSvnCommand argument is mishandled.'}]
-                })
+            'py-1.11.0-py2.py3-none-any.whl:CVE-2022-42969': [
+                {'name': 'py-1.11.0-py2.py3-none-any.whl:CVE-2022-42969', 'file': 'py-1.11.0-py2.py3-none-any.whl', 'code': 'CVE', 'severity': 'HIGH', 'score': 7.5, 'detail': 'The py library through 1.11.0 for Python allows remote attackers to conduct a ReDoS (Regular expression Denial of Service) attack via a Subversion repository with crafted info data, because the InfoSvnCommand argument is mishandled.'}]
+        })
 
 
 class ConvertTest(TestCase):
@@ -71,5 +75,5 @@ class ConvertTest(TestCase):
         self.assertXmlIsValid(self.destination)
 
     def test_should_create_a_file_even_when_there_are_no_errors(self):
-         _convert(valid_mend, self.destination)
-         self.assertFileExist(self.destination)
+        _convert(valid_mend, self.destination)
+        self.assertFileExist(self.destination)
