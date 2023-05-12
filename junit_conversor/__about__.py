b4ccbb2edf9cf8e97ee28745d5c5164a0eb6bd26
michelle.taal@shell.com <michelle.taal@shell.com>
2023-05-12 12:22:23 +1000

    set version

--- junit_conversor/__about__.py
+++ junit_conversor/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'mend-junit-report'
 __summary__ = 'Simple tool that converts a mend file to junit format'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __uri__ = 'https://github.com/sede-x/mend-junit-report'
 __author__ = 'Michelle A. Taal'
 __email__ = 'michelletaal@shell.com'
